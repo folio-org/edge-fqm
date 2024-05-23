310b4c96cbd16d90b4ddba44a3b573b6a3032fc4
Taras Spashchenko <41001394+TarasSpashchenko@users.noreply.github.com>
2024-05-23 15:37:11 +0300

    Merge pull request #25 from folio-org/EDGFQM-19
    Update TLS configuration and dependencies

--- src/main/java/org/folio/fqm/edge/EdgeFqmApplication.java
+++ src/main/java/org/folio/fqm/edge/EdgeFqmApplication.java
@@ -1,14 +1,22 @@
 package org.folio.fqm.edge;
 
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.security.Security;
+
+import static org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider.PROVIDER_NAME;
 
 @SpringBootApplication
 @EnableFeignClients
 public class EdgeFqmApplication {
 
   public static void main(String[] args) {
+    if (Security.getProvider(PROVIDER_NAME) == null) {
+      Security.addProvider(new BouncyCastleFipsProvider());
+    }
     SpringApplication.run(EdgeFqmApplication.class, args);
   }
 
